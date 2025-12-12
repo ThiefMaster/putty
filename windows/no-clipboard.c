eb27699e336a649f5901da2cf51c1abb544ea95e
Adrian Moennich <adrian@planetcoding.net>
2025-12-12 01:01:10 +0100

    Fix failing test builds

---
+++ windows/no-clipboard.c
@@ -0,0 +1,8 @@
+/*
+ * no-clipboard.c: stub clipboard functions for Windows executables
+ * that don't update the clipboard.
+ */
+
+#include "putty.h"
+
+void write_aclip(HWND hwnd, int clipboard, char *, int) {}
